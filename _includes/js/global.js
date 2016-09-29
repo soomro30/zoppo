c8304735c0c090dc8522d65ce898e50028e39a0f
Jonas Sandstedt <jonas@fullyflared.se>
2016-09-29 20:20:21 +0200

    integrated barba.js with GSAP animation

---
+++ _includes/js/global.js
@@ -0,0 +1,108 @@
+// Init all code on document eady
+function ready(fn) {
+	if (document.readyState != 'loading') {
+		fn();
+	} else {
+		document.addEventListener('DOMContentLoaded', fn);
+	}
+}
+ready(function(){
+
+	// Initialize barba.js
+	Barba.Pjax.start();
+
+	// //simple hide show animation
+	// var HideShowTransition = Barba.BaseTransition.extend({
+	//   start: function() {
+	//     this.newContainerLoading.then(this.finish.bind(this));
+	//     console.log('start');
+	//   },
+
+	//   finish: function() {
+	//     document.body.scrollTop = 0;
+	//     this.done();
+	//     console.log('finsh');
+	//   }
+	// });
+
+	// Barba.Pjax.getTransition = function() {
+	//   return HideShowTransition;
+	// };
+	// 
+	// 
+
+	// simple fade animation
+	var FadeTransition = Barba.BaseTransition.extend({
+		start: function() {
+		/**
+		* This function is automatically called as soon the Transition starts
+		* this.newContainerLoading is a Promise for the loading of the new container
+		* (Barba.js also comes with an handy Promise polyfill!)
+		*/
+
+		// As soon the loading is finished and the old page is faded out, let's fade the new page
+		Promise
+			.all([this.newContainerLoading, this.animOut()])
+			.then(this.animIn.bind(this));
+		},
+
+		/**
+		 * [animOut transition]
+		 * @return { promise }
+		 */
+		animOut: function() {
+
+			var deferred = Barba.Utils.deferred();
+
+			/**
+			 * Animate out the old container 
+			 * this.oldContainer is the HTMLElement of the old Container
+			 */
+			TweenMax.to(this.oldContainer, 0.4, {
+				opacity: 0,
+				onComplete: function() {
+					deferred.resolve();
+				}
+			});
+
+			return deferred.promise;
+		},
+		/**
+		 * [animIn transition]
+		 * @return { promise }
+		 */
+		animIn: function() {
+			var _this = this;
+			var newCont = this.newContainer;
+
+			// Hide the old container
+			this.oldContainer.style.display = "none";
+
+			// Set the new container's start values
+			TweenMax.set(newCont, { visibility: 'visible', opacity:0 });
+
+			// Animate in the new container
+			TweenMax.to(newCont, 0.4, {
+				opacity: 1,
+				onComplete: function() {
+					// .done() will automatically remove the old Container from the DOM 
+					_this.done();
+				}
+			});
+		}
+	});
+
+	/**
+	* Next step, you have to tell Barba to use the new Transition
+	*/
+
+	Barba.Pjax.getTransition = function() {
+	/**
+	* Here you can use your own logic!
+	* For example you can use different Transition based on the current page or link...
+	*/
+
+	return FadeTransition;
+	};
+
+});
