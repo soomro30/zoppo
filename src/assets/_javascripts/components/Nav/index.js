4c243ba61c6ec490b2383d2b5d7790fdb4a5c738
Jonas Sandstedt <jonas@fullyflared.se>
2017-09-05 17:54:07 +0200

    added tab navigation support

--- src/assets/_javascripts/components/Nav/index.js
+++ src/assets/_javascripts/components/Nav/index.js
@@ -12,18 +12,32 @@ class Nav {
 			this._menuIsOpen ? this.closeMenu() : this.openMenu();
 		}, false);
 		this.logoAnimation = new LogoAnimation;
-    }
+		// Set interactive elements
+		
+		this.menuItems = document.querySelectorAll('#menu a');
+		this.setTabIndex(-1, this.menuItems);
+	}
+	
+	// Functionality
+	setTabIndex (index, items) {
+		for(var i = 0; i < items.length; i++) {
+			items[i].tabIndex = index;
+		}
+		console.log('set tab index', items);
+	}
 
 	openMenu () {
 		this._menutoggle.classList.add('menu-open');
 		this._menu.classList.add('menu-open');
 		this.logoAnimation.startLogoAnimation();
 		this._menuIsOpen = true;
+		this.setTabIndex(0, this.menuItems);
 	}
 	closeMenu () {
 		this._menutoggle.classList.remove('menu-open');
 		this._menu.classList.remove('menu-open');
 		this._menuIsOpen = false;
+		this.setTabIndex(-1, this.menuItems);
 	}
 
 }
