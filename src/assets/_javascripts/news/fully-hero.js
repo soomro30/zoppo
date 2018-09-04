7f0ce9aa655715e497e10e459d9effe765dd2238
Sandstedt <kontakta@jonassandstedt.se>
2018-09-04 19:32:28 +0200

    Added comments on fully-hero and fallback fix

--- src/assets/_javascripts/news/fully-hero.js
+++ src/assets/_javascripts/news/fully-hero.js
@@ -1,18 +1,114 @@
 // import "babel-polyfill";
 import lottie from 'lottie-web';
 
+/**
+ * Create a Lottie scene from data-attributes: data-scene, data-path and data-preserveaspectratio
+ * 
+ * @param {object} node - The lottie DOM container:
+ * @method init [frame = 0] - Plays the scene, adds classes and limit framerate
+ * @method pause - Pauses the scene
+ * @method destroy - Destroy the scene and add a fallback image class
+ * @method resize - Resize the scene (when using canvas rendering)
+ * @method slowMo - Slow motion function on hover
+ * @method loopBetween [segment, force] - Loops between the first and second value in the segment array
+ * @method hoverPlayFrom [frame = 0] - Start the animation on hover from a certain frame
+ */
+class Scene {
+  constructor(node) {
+    this.node = node;
+    const dataset = node.dataset;
+    this.path = dataset.path;
+    this.scene = dataset.scene
+    this.preserveaspectratio = dataset.preserveaspectratio;
+    this.loaded = false;
+
+    // The lottie animation instance
+    this.anim = lottie.loadAnimation(
+      {
+        container: this.node,
+        renderer: 'canvas', // canvas works better on firefox
+        loop: true,
+        autoplay: false,
+        path: `../../assets/news/fully-hero/${this.path}`,
+        rendererSettings: { preserveAspectRatio: this.preserveaspectratio }
+      }
+    );
+  }
+
+  init(frame = 0) {
+    this.anim.goToAndPlay(frame, true);
+    this.node.classList.remove('fallback');
+    this.node.classList.add('loaded');
+    limitFps && this.anim.setSubframe(false); // run in 30fps
+  }
+
+  pause() {
+    this.anim.pause();
+  }
+
+  destroy() {
+    this.anim.destroy();
+    this.node.classList.add('fallback');
+  }
+
+  resize() {
+    this.anim.resize();
+  }
+
+  // Slow motion feature
+  slowMo() {
+    this.node.addEventListener('mouseover', () => {
+      limitFps && this.anim.setSubframe(true); // interpolate the keyframes in between
+      this.anim.setSpeed(0.05);
+    });
+    this.node.addEventListener('mouseout', () => {
+      limitFps && this.anim.setSubframe(false); // run in 30fps
+      this.anim.setSpeed(1)
+    });
+  }
+
+  // segment: array with to values, from and to
+  // force: true if you wanna force jump tp the startframe
+  loopBetween(segment, force) {
+    if (!this.loaded) {
+      this.anim.addEventListener('DOMLoaded', () => {
+        this.loaded = true;
+        this.loopBetween(segment, force);
+      });
+      return;
+    }
+
+    this.anim.playSegments(segment, force);
+  }
+
+  // play from frame on hover
+  // hoverFrame: the frame you want to go to and play
+  hoverPlayFrom(hoverFrame = 0) {
+    if (!this.loaded) {
+      this.anim.addEventListener('DOMLoaded', () => {
+        this.loaded = true;
+        this.hoverPlayFrom(hoverFrame);
+      });
+      return;
+    }
+    this.node.addEventListener('mouseover', () => {
+      this.anim.goToAndPlay(hoverFrame, true);
+    });
+  }
+}
+
 const defaultHero = () => {
+  const limitFps = true; // limit to AfterEffects fps (eg 30fps)
+  let anims = []; // an array of all the lottie anim instances
+
   // Get both hero in firstpage / news and menu
   const fullyheros = document.querySelectorAll('[data-news="fully-hero"]');
-
-  if (!fullyheros || fullyheros.length <= 1) return;
+  if (!fullyheros) return;
 
   // only animate the one hero for performence reasons
   // and set fallback on the other on (if there is two)
   const hero = fullyheros.length > 1 ? fullyheros[1] : fullyheros[0];
-  if (fullyheros.length === 1) {
-    return;
-  }
+
   let fallbacks = [];
   if (fullyheros.length > 1 || fullyheros.length === 1) {
     fallbacks = [...fullyheros[0].querySelectorAll('[data-scene]')];
@@ -35,97 +131,14 @@ const defaultHero = () => {
     fallback.classList.add('fallback');
   });
 
-  const limitFps = true; // limit to AfterEffects fps (eg 30fps)
-  let anims = [];
+  if (fullyheros.length <= 1) return;
 
-  class Scene {
-    constructor(node) {
-      this.node = node;
-      const dataset = node.dataset;
-      this.path = dataset.path;
-      this.scene = dataset.scene
-      this.preserveaspectratio = dataset.preserveaspectratio;
-      this.loaded = false;
-
-      // The lottie animation instance
-      this.anim = lottie.loadAnimation(
-        {
-          container: this.node,
-          renderer: 'canvas', // canvas works better on firefox
-          loop: true,
-          autoplay: false,
-          path: `../../assets/news/fully-hero/${this.path}`,
-          rendererSettings: { preserveAspectRatio: this.preserveaspectratio }
-        }
-      );
-    }
-
-    init(frame = 0) {
-      this.anim.goToAndPlay(frame, true);
-      this.node.classList.remove('fallback');
-      this.node.classList.add('loaded');
-      limitFps && this.anim.setSubframe(false); // run in 30fps
-    }
-
-    pause() {
-      this.anim.pause();
-    }
-
-    destroy() {
-      this.anim.destroy();
-      this.node.classList.add('fallback');
-    }
-
-    resize() {
-      this.anim.resize();
-    }
-
-    // Slow motion feature
-    slowMo() {
-      this.node.addEventListener('mouseover', () => {
-        limitFps && this.anim.setSubframe(true); // interpolate the keyframes in between
-        this.anim.setSpeed(0.05);
-      });
-      this.node.addEventListener('mouseout', () => {
-        limitFps && this.anim.setSubframe(false); // run in 30fps
-        this.anim.setSpeed(1)
-      });
-    }
-
-    // segment: array with to values, from and to
-    // force: true if you wanna force jump tp the startframe
-    loopBetween(segment, force) {
-      if (!this.loaded) {
-        this.anim.addEventListener('DOMLoaded', () => {
-          this.loaded = true;
-          this.loopBetween(segment, force);
-        });
-        return;
-      }
-
-      this.anim.playSegments(segment, force);
-    }
-
-    // play from frame on hover
-    // hoverFrame: the frame you want to go to and play
-    hoverPlayFrom(hoverFrame) {
-      if (!this.loaded) {
-        this.anim.addEventListener('DOMLoaded', () => {
-          this.loaded = true;
-          this.hoverPlayFrom(hoverFrame);
-        });
-        return;
-      }
-      this.node.addEventListener('mouseover', () => {
-        this.anim.goToAndPlay(hoverFrame, true);
-      });
-    }
-  }
-
+  // Use fallbacks images instead of animations
+  // in case the user preferes a reduced motion experience
   let motionQuery = matchMedia('(prefers-reduced-motion)');
   function handleReduceMotionChanged() {
     if (motionQuery.matches) {
-      /* adjust motion of 'transition' or 'animation' properties */
+      // Reduced motion
       const allScenes = [...hero.querySelectorAll('[data-scene]')];
 
       allScenes.map(scene => {
@@ -135,7 +148,7 @@ const defaultHero = () => {
         anim.destroy();
       })
     } else {
-      /* standard motion */
+      // standard motion
       if (window.matchMedia("(orientation: landscape)").matches) {
 
         const scene1 = new Scene(hero.querySelector('[data-scene="1"]'));
@@ -147,6 +160,7 @@ const defaultHero = () => {
         scene2.slowMo();
         anims.push(scene2);
 
+        // TODO: Make a button to hide navigation and show this scene
         // const scene4 = new Scene(scenes[3]);
         // scene4.init();
         // anims.push(scene4);
